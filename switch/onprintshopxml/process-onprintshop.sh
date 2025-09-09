8967931a43d4063c3763a4e20ca42d2c181b8316
switchbox <switchbox@switchbox-2.local>
2025-09-09 11:38:25 +1000

    Update process-onprintshop.sh and process-jobs.sh to accept XML path as argument

---
+++ switch/onprintshopxml/process-onprintshop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+XML_PATH="$1"
+LOG_FILE="/Users/Shared/Next-Printing/Resources/Switch to Posgres/switch/onprintshopxml/onprintshop.log"
+NODE_PATH="/usr/local/bin/node"
+SCRIPT_PATH="/Users/Shared/Next-Printing/Resources/Switch to Posgres/switch/onprintshopxml/parse-onprintshop.js"
+
+
+# Logging clearly what was passed in
+echo "=======================" >> "$LOG_FILE"
+echo "XML_PATH: $XML_PATH" >> "$LOG_FILE"
+echo "Timestamp: $(date)" >> "$LOG_FILE"
+echo "=======================" >> "$LOG_FILE"
+
+"$NODE_PATH" "$SCRIPT_PATH" "$XML_PATH"
